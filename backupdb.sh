4794dd9447a291d50a8e08664f9a228cf3aa76b3
Thirumurthy <91thiru@gmail.com>
2020-04-02 20:34:36 +0530

    initial commit

---
+++ backupdb.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+cd $(dirname $0)
+DB=$1
+DBUSER=$2
+DBPASSWD=$3
+FILE=$DB-$(date +%F).sql
+mysqldump --routines "--user=${DBUSER}"  --password=$DBPASSWD $DB > $PWD/$FILE
+gzip $FILE
+echo Created $PWD/$FILE*
+# Code Copied from https://stackoverflow.com/questions/5075198/how-to-export-mysql-database-with-triggers-and-procedures
+# Example Usage : backupdb.sh my_db dev_user dev_password
